d52b1340a5ecfcc1c7fc745e9d337587df8de471
Toprak Yasin KESKİN <y.keskinn@gmail.com>
2026-07-12 10:21:36 +0300

    Initial commit

---
+++ KirmiziFil.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KirmiziFil.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
